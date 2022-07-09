a1b87a82badb4a0406ee43c2837da09a4896be17
Ivan Notaros <nothke@gmail.com>
2022-07-09 03:25:58 +0200

    No longer applying color in Blender 3.2 or later & added option to skip conversion if an object has no nodes

--- attribute_converter.py
+++ attribute_converter.py
@@ -24,25 +24,40 @@ class NOTHKE_OT_AttributeConverter(bpy.types.Operator):
     bl_idname = "object.attrcon_apply"
     bl_label = "Convert All Attributes"
 
-    uv_name: StringProperty(default="uv")
-    color_name: StringProperty(default="color")
+    uv_name: StringProperty(default="UVMap")
+    color_name: StringProperty(default="Color")
     remove_existing: BoolProperty(default=True)
+    skip_if_no_nodes: BoolProperty(default=True)
 
     def execute(self, context):
         print("Executing")
 
+        # note: in blender 3.2+ vertex colors no longer need to be converted
+        #       as they are shared between "Attributes" and "Color Attributes"
+        version = bpy.app.version
+        b_3_2_or_better = version[0] >= 3 and version[1] >= 2
+
         for obj in bpy.context.selected_objects:
             context.view_layer.objects.active = obj
 
-            # apply modifiers
+            has_geonodes = False
+            for modifier in obj.modifiers:
+                print(modifier.type)
+                if modifier.type == 'NODES':
+                    has_geonodes = True
+
+            if self.skip_if_no_nodes and not has_geonodes:
+                print("object " + obj.name + " has no geometry nodes modifier, skipping")
+                continue
+
             bpy.ops.object.convert(target='MESH')
 
             # remove existing uv and color maps
             if self.remove_existing:
-                clear_collection(obj.data.vertex_colors)
                 clear_collection(obj.data.uv_layers)
+                if not b_3_2_or_better:
+                    clear_collection(obj.data.vertex_colors)
 
-            # convert attributes
             for i in reversed(range(len(obj.data.attributes))):
                 attr = obj.data.attributes[i]
 
@@ -53,7 +68,7 @@ class NOTHKE_OT_AttributeConverter(bpy.types.Operator):
                 if attr.name == self.uv_name:
                     bpy.ops.geometry.attribute_convert(mode='UV_MAP')
 
-                if attr.name == self.color_name:
+                if attr.name == self.color_name and not b_3_2_or_better:
                     bpy.ops.geometry.attribute_convert(mode='VERTEX_COLOR')
 
         return {'FINISHED'}
@@ -75,6 +90,9 @@ class NOTHKE_PT_AttributeConverter(bpy.types.Panel):
         layout = self.layout
 
         row = layout.row()
+        row.prop(scene, 'attrcon_skip_if_no_nodes')
+
+        row = layout.row()
         row.prop(scene, 'attrcon_remove_existing')
 
         row = layout.row()
@@ -91,6 +109,7 @@ class NOTHKE_PT_AttributeConverter(bpy.types.Panel):
         op.uv_name = scene.attrcon_uv_name
         op.color_name = scene.attrcon_color_name
         op.remove_existing = scene.attrcon_remove_existing
+        op.skip_if_no_nodes = scene.attrcon_skip_if_no_nodes
 
 
 def register():
@@ -114,6 +133,11 @@ def register():
         description="Do you want to remove existing uv and color maps before applying the converted attributes? If false, it will keep the original and add the converted attributes.",
         default=True)
 
+    bpy.types.Scene.attrcon_skip_if_no_nodes = bpy.props.BoolProperty(
+        name="Skip if no nodes",
+        description="",
+        default=True)
+
     print('properties registered')
 
     bpy.utils.register_class(NOTHKE_PT_AttributeConverter)
@@ -127,6 +151,7 @@ def unregister():
     del bpy.types.Scene.attrcon_uv_name
     del bpy.types.Scene.attrcon_color_name
     del bpy.types.Scene.attrcon_remove_existing
+    del bpy.types.Scene.attrcon_skip_if_no_nodes
 
 
 if __name__ == "__main__":
